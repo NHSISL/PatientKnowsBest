247a7e904ae6352f40b11638189a7f4fe4cc7085
Christo du Toit <christo@dutoit.co.uk>
2025-10-13 11:56:56 +0100

    INFRA: Initial checkin

---
+++ PatientKnowsBest.Infrastructure/Program.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using PatientKnowsBest.Infrastructure.Services;
+
+namespace PatientKnowsBest.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+
+            scriptGenerationService.GenerateBuildScript(
+                branchName: "main",
+                projectName: "PatientKnowsBest.Core",
+                dotNetVersion: "9.0.100");
+
+            scriptGenerationService.GeneratePrLintScript(branchName: "main");
+        }
+    }
+}
